e8f7d6e94280e79a0d6bc2f8040447c7a6d4c453
Deepanshu Johar <deepanshujohar46@gmail.com>
2021-12-31 00:08:47 +0530

    offline-vscode-template

---
+++ offline-template.cpp
@@ -0,0 +1,101 @@
+
+// #include<bits/stdc++.h>
+
+// /**
+#include<iostream>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <unordered_set>
+// */
+
+
+using namespace std;
+#define fast_io {\
+                ios_base::sync_with_stdio(false);\
+                cin.tie(0);\
+                cout.tie(0);}
+#define rep(i,a,b) for(int i=a ; i<(b) ; ++i )
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+using ll = long long;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+const int MOD = 1e9 + 7;
+
+void solve(){
+    cout<<"Hello World!!";
+}
+
+signed main() {
+    
+    //OPTIMIZE
+
+    // freopen("in.txt" , "r" , stdin) ;
+    // freopen("out.txt" , "w" , stdout) ;
+	
+    fast_io;
+	int t=1;
+	cin>>t;
+	while(t--)
+	{
+	    solve();
+	}
+	return 0;
+}
